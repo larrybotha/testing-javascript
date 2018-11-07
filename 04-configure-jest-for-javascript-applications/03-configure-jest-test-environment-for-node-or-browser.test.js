d21bb9f73359d8a3f46cac10d39879bd3f6e8867
Larry Botha <larry@fixate.it>
2018-11-07 08:46:57 +0200

    add test

---
+++ 04-configure-jest-for-javascript-applications/03-configure-jest-test-environment-for-node-or-browser.test.js
@@ -0,0 +1,11 @@
+import {getFormattedValue} from './src/utils';
+
+test('formats the value', () => {
+  expect(getFormattedValue('1234.0')).toBe('1,234.0');
+});
+
+// we have access to window, despite being in a node environment, because Jest
+// makes use of jsdom by default for us
+// We can configure Jest to run exclusively for the browser, or node, via
+// jest.config.js
+console.log(window);
