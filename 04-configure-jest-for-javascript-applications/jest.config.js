0ce6ababb5493fb0658111ad306361bce086be9c
Larry Botha <larry@fixate.it>
2018-11-07 09:13:11 +0200

    map css imports in tests to an empty module

--- 04-configure-jest-for-javascript-applications/jest.config.js
+++ 04-configure-jest-for-javascript-applications/jest.config.js
@@ -2,4 +2,12 @@ module.exports = {
   // To run Jest without jsdom, we use jest-environment-node
   // testEnvironment: 'jest-environment-node',
   testEnvironment: 'jest-environment-jsdom',
+
+  moduleNameMapper: {
+    // attempting to import .css files with Node will error, because Node
+    // expects the files to be Node modules.
+    // To get around this, we configure Jest to resolve to a file we specify
+    // whenever a file ends in .css
+    '\\.css$': require.resolve('./test/style-mock.js'),
+  },
 };
