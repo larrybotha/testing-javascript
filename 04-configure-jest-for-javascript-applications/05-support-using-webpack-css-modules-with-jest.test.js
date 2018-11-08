51813ace5ee33066e02eed315f31b3558d27a2b7
Larry Botha <larry@fixate.it>
2018-11-08 09:00:53 +0200

    add test and annotations for using identity-obj-proxy

---
+++ 04-configure-jest-for-javascript-applications/05-support-using-webpack-css-modules-with-jest.test.js
@@ -0,0 +1,14 @@
+import 'react-testing-library/cleanup-after-each';
+import React from 'react';
+import {render} from 'react-testing-library';
+
+import AutoScalingText from './src/auto-scaling-text';
+
+test('renders', () => {
+  const {container} = render(<AutoScalingText />);
+  // because we're mocking out css imports, the className on AutoScalingText
+  // will be undefined, and we won't see a class output on the component
+  // By using identity-obj-proxy in Jest's moduleNameMapper we can have a more
+  // descriptive output for the className
+  console.log(container.innerHTML);
+});
