4df653aad920dc9105fd2b6b0782f3d08ccdcb0e
Larry Botha <larry@fixate.it>
2018-11-09 09:44:20 +0200

    add babel-plugin-dynamic-import-node

--- 04-configure-jest-for-javascript-applications/.babelrc.js
+++ 04-configure-jest-for-javascript-applications/.babelrc.js
@@ -1,10 +1,13 @@
+const isTest = process.env.NODE_ENV === 'test';
+
 module.exports = {
   presets: [
-    [
-      '@babel/preset-env',
-      {modules: process.env.NODE_ENV === 'test' ? 'commonjs' : false},
-    ],
+    ['@babel/preset-env', {modules: isTest ? 'commonjs' : false}],
     '@babel/preset-react',
   ],
-  plugins: ['@babel/plugin-proposal-class-properties', 'emotion'],
+  plugins: [
+    '@babel/plugin-proposal-class-properties',
+    'emotion',
+    isTest ? 'babel-plugin-dynamic-import-node' : null,
+  ].filter(Boolean),
 };
