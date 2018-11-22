0bf12d49216f1a6c98080c59121cba0f1cd12776
Larry Botha <larry@fixate.it>
2018-11-22 10:44:09 +0200

    add test for calculator when user is authenticated

--- 05-install-configure-and-script-cypress-for-javaScript-web-applications/cypress/e2e/calculator.js
+++ 05-install-configure-and-script-cypress-for-javaScript-web-applications/cypress/e2e/calculator.js
@@ -18,3 +18,32 @@ describe('anonymous calculator', () => {
       .should('have.text', '3')
   })
 })
+
+// test the calculaor for an authenticated user
+describe('authenticated calculator', () => {
+  it(`displays the user's name`, () => {
+    cy.createUser().then(user => {
+      // log the user in
+      cy.visit('/')
+        .getByText(/login/i)
+        .click()
+        .getByLabelText(/username/i)
+        .type(user.username)
+        .getByLabelText(/password/i)
+        .type(user.password)
+        .getByText(/submit/i)
+        .click()
+
+        // assert that they are in fact logged in
+        .assertLoggedInAs(user)
+
+        // log the user out
+        .getByText(/logout/i)
+        .click()
+
+        // assert that the user's display name is no longer in the DOM
+        .queryByText('username-display', {timeout: 300})
+        .should('not.exist')
+    })
+  })
+})
