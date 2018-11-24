cc059311c4b9e13953709bf7508585f086dc9c23
Larry Botha <larry@fixate.it>
2018-11-24 14:29:45 +0200

    add assertions on rendered component

--- 06-test-react-components-with-jest-and-react-testing-library/__tests__/favorite-number.test.js
+++ 06-test-react-components-with-jest-and-react-testing-library/__tests__/favorite-number.test.js
@@ -4,9 +4,12 @@ import ReactDOM from 'react-dom';
 import {FavoriteNumber} from '../src/favorite-number';
 
 describe('FavoriteNumber', () => {
-  test('renders a number input with a label "Favourite Number"', () => {
+  test('renders a number input with a label "Favorite Number"', () => {
     const div = document.createElement('div');
 
     ReactDOM.render(<FavoriteNumber />, div);
+
+    expect(div.querySelector('input').type).toBe('number');
+    expect(div.querySelector('label').textContent).toBe('Favorite Number');
   });
 });
