0dc1f7c1c0fe916d5fd7347d2f88a53f96eaabe0
Larry Botha <larry@fixate.it>
2018-12-04 10:53:18 +0200

    add app and tests

---
+++ 06-test-react-components-with-jest-and-react-testing-library/__tests__/redux-app-01.test.js
@@ -0,0 +1,32 @@
+import 'jest-dom/extend-expect';
+import 'react-testing-library/cleanup-after-each';
+
+import React from 'react';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import {render, fireEvent} from 'react-testing-library';
+
+import {ConnectedCounter, reducer} from '../src/redux-app-01';
+
+describe('ConnectedCounter', () => {
+  test('renders with redux defaults', () => {
+    const store = createStore(reducer);
+    const {getByText, getByTestId} = render(
+      <Provider store={store}>
+        <ConnectedCounter />
+      </Provider>
+    );
+    expect(getByTestId('count-value')).toHaveTextContent(0);
+
+    const incButton = getByText('+');
+    const decButton = getByText('-');
+
+    fireEvent.click(incButton);
+
+    expect(getByTestId('count-value')).toHaveTextContent(1);
+
+    fireEvent.click(decButton);
+
+    expect(getByTestId('count-value')).toHaveTextContent(0);
+  });
+});
