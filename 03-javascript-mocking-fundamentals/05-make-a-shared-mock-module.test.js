c4b4491d0cd5ba30f0f5176bb7b66843678f3814
Alex Grant <alexmgrant@me.com>
2020-11-29 20:20:11 -0500

    Update 05-make-a-shared-mock-module.test.js
    Fixed the misspelling of 'moving'

--- 03-javascript-mocking-fundamentals/05-make-a-shared-mock-module.test.js
+++ 03-javascript-mocking-fundamentals/05-make-a-shared-mock-module.test.js
@@ -1,7 +1,7 @@
 const thumbWar = require('./src/thumb-war');
 const utils = require('./src/utils');
 
-// instead of movking a module on a per file basis, we can share the mock
+// instead of moving a module on a per file basis, we can share the mock
 // implementation using a __mocks__ folder with a file for the module exporting
 // the mock implementation
 // jest.mock('./src/utils', () => {
